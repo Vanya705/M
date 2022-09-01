0a96722c5d002e80fabe060a46440735a58392e0
Ivan <example@gmail.com>
2022-09-01 16:49:12 +0400

    first commit

---
+++ Новая папка/index.js
@@ -0,0 +1,111 @@
+"use strict";
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+
+// //Объект
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.age);
+// //console.log(obj["name"]);
+
+// // Массив
+// let arr = ["plum.png", "orange.png", 6, {}, ];
+
+// console.log(arr[0]);
+
+
+// const arr = [1, 2, 3];
+
+// arr[4] = 5;
+
+// console.log(arr);
+
+// const arrObj = {
+//     0: 1,
+//     1: 2,
+//     2: 3
+// };
+
+// arrObj.a = 5;
+
+// // console.log(arr[0]);
+// console.log(arrObj.a);
+
+
+// // const arr1 = {a: 1, b: 2};
+
+// const arr1 = {
+//         Anna: 500,
+//         Bob: 600
+// };
+
+// 
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18", "18");
+// // console.log(typeof(answer));
+// console.log(answer + 5); // прибавили строку к числу
+
+// const answer1 = +prompt("Вам есть 18", "18");
+// console.log(answer1 + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя,", "");
+// answers[1] = prompt("Как ваша фамилия,", "");
+// answers[2] = prompt("Сколько вам лет,", "");
+
+// document.write(answers);
+
+
+//Интерполяция
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+console.log("arr" + " = object");
+
+console.log(4 + " - object");
+
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++; //инкремент(увеличение значения на 1)
+// decr--; //дикремент(уменьшение значения на 1)
+
+// console.log(incr++); //значения остаются теми же(10)
+// console.log(decr++);
+
+// console.log(++incr); //значения меняются
+// console.log(--decr);
+
+console.log(2*4 === 8);
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose); //И
+console.log(isChecked || isClose); //Или
+
+
+
+
+
+
+
+
